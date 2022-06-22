dff1a7ae836e69bdc3a216e0422f9d07aff4a60a
ken0901 <ken5260715@gmail.com>
2022-06-21 21:06:49 -0700

    Refactoring codes

--- src/component/CreateWord.js
+++ src/component/CreateWord.js
@@ -1,35 +1,40 @@
-import { useRef } from "react";
+import { useRef, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import useFetch from "../hooks/useFetch";
 
 export default function CreateWord(){
     const days = useFetch("http://localhost:3001/days");
     const navigate = useNavigate();
+    const [isLoading, setIsLoading] = useState(false);
 
     function onSubmit(e){
         e.preventDefault();
 
-        fetch(`http://localhost:3001/words/`,{
-            method:'POST',
-            headers:{
-                'content-Type': 'application/json',
-            },
-            body: JSON.stringify({
-                day:dayRef.current.value,
-                eng:engRef.current.value,
-                kor:korRef.current.value,
-                isDone: false,
-            }),
-        }).then(res=>{
-            if(res.ok){
-                alert('Successfully Saved');
-                navigate(`/day/${dayRef.current.value}`);
-            }
-        });
+        if(!isLoading){
+            setIsLoading(true);
+            fetch(`http://localhost:3001/words/`,{
+                method:'POST',
+                headers:{
+                    'content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                    day:dayRef.current.value,
+                    eng:engRef.current.value,
+                    meaning:meaningRef.current.value,
+                    isDone: false,
+                }),
+            }).then(res=>{
+                if(res.ok){
+                    alert('Successfully Saved');
+                    navigate(`/day/${dayRef.current.value}`);
+                    setIsLoading(false);
+                }
+            });
+        }
     }
 
     const engRef = useRef(null);
-    const korRef = useRef(null);
+    const meaningRef = useRef(null);
     const dayRef = useRef(null);
 
     return(
@@ -39,8 +44,8 @@ export default function CreateWord(){
                 <input type="text" placeholder="computer" ref={engRef}/>
             </div>
             <div className="input_area">
-                <label>Korean</label>
-                <input type="text" placeholder="컴퓨터" ref={korRef}/>
+                <label>Meaning</label>
+                <input type="text" placeholder="It means ..." ref={meaningRef}/>
             </div>
             <div className="input_area">
                 <label>Day</label>
@@ -50,7 +55,12 @@ export default function CreateWord(){
                     ))}
                 </select>
             </div>
-            <button>Save</button>
+            <button
+                style={{
+                    opacity: isLoading ? 0.3 : 1,
+                }}
+            >
+                {isLoading ? "Saving..." : "Save"}</button>
         </form>
     );
 }
